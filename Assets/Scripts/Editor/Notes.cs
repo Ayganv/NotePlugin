da35137be7ba4810c8072ef3382b408d5d494b6f
Linus Malmström <augonous@gmail.com>
2021-01-12 14:12:43 +0100

    Creates Inspector Window

---
+++ Assets/Scripts/Editor/Notes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Note : EditorWindow
+{
+    
+    [MenuItem("Window/Note")]
+    static void Init()
+    {
+       
+        Note window = (Note)EditorWindow.GetWindow(typeof(Note));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Notes", EditorStyles.boldLabel);
+    
+        
+        if (GUILayout.Button("Create"))
+        {
+            
+        }
+    }
+}
