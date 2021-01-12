e696cedd50b1b27fd8896f997c397d7acab68160
Linus Malmström <augonous@gmail.com>
2021-01-12 14:25:06 +0100

    Adds Note Component, MonoBehaviour

---
+++ Assets/Scripts/NoteComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteComponent : MonoBehaviour
+{
+    [TextArea(10, 10)]
+    public string Note;
+    public string tags;
+    public Color color;
+}
