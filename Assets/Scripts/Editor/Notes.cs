899cae4d0540ecef3c8c66be226e2916a3c1c0e7
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-01-13 11:33:05 +0100

    Update Notes.cs
    Scrollable display of GameObjects in EditorWindow, Adds Refresh button & functionality to refresh the sceneNotes List,

--- Assets/Scripts/Editor/Notes.cs
+++ Assets/Scripts/Editor/Notes.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 public class Note : EditorWindow
 {
+    
+    private const float padding = 10;
+    private float hsBar;
+    private List<NoteComponent> sceneNotes;
+    private Vector2 scroll;
+    
+    
     
     [MenuItem("Window/Note")]
     static void Init()
@@ -12,14 +20,68 @@ public class Note : EditorWindow
         window.Show();
     }
 
+    private void OnEnable()
+    {
+        Refresh();
+    }
+
     void OnGUI()
     {
-        GUILayout.Label("Notes", EditorStyles.boldLabel);
-    
         
+        GUILayout.Label("Notes", EditorStyles.boldLabel);
+
+        GUILayout.BeginVertical();
+        //hsBar = GUILayout.VerticalScrollbar(hsBar, 0f,0f,10f);
+
+        scroll = GUILayout.BeginScrollView(scroll);
+        
+        //Iterate through every sceneNote in the List and output what we need to get from it
+        for (int index = 0; index < this.sceneNotes.Count; index++)
+        {
+            //Add here what we want to show for each Component Object
+            
+            GUILayout.BeginVertical();
+            GUILayout.Space(padding);
+            GUILayout.Label("NoteInScene", EditorStyles.boldLabel);
+            GUILayout.Label(sceneNotes[index].Note);
+            GUILayout.Space(padding);
+            GUILayout.EndVertical();
+        }
+        
+        GUILayout.EndScrollView();
+        
+        GUILayout.EndVertical();
+      
+        GUILayout.BeginHorizontal();
         if (GUILayout.Button("Create"))
         {
             
         }
+        
+        GUILayout.FlexibleSpace();
+        
+        if (GUILayout.Button("Refresh List"))
+        {
+            
+            Refresh();
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    public void Refresh()
+    {
+        this.sceneNotes = new List<NoteComponent>();
+        foreach (GameObject gameObject in (GameObject[]) Object.FindObjectsOfType<GameObject>())
+        {
+            NoteComponent[] components = (NoteComponent[]) gameObject.GetComponents<NoteComponent>();
+            if (components != null)
+            {
+                foreach (NoteComponent noteComponent in components)
+                {
+                    this.sceneNotes.Add(noteComponent);
+                }
+            }
+        }
+        this.Repaint();
     }
 }
