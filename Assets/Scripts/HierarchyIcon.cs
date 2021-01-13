c32ef79de0dcdecaec5e5ae65ca412f22de91e21
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2021-01-13 10:24:29 +0100

    adds a hierarchy icon

---
+++ Assets/Scripts/HierarchyIcon.cs
@@ -0,0 +1,48 @@
+﻿ using UnityEditor;
+ using UnityEngine;
+ using System.Collections.Generic;
+ 
+ [InitializeOnLoad]
+ class HierarchyIcon
+ {
+     static Texture2D texture;
+     static List<int> markedObjects;
+     
+     static HierarchyIcon ()
+     {
+         // Init
+         texture = AssetDatabase.LoadAssetAtPath ("Assets/Images/note.png", typeof(Texture2D)) as Texture2D;
+         EditorApplication.update += UpdateCB;
+         EditorApplication.hierarchyWindowItemOnGUI += HierarchyItemCB;
+     }
+     
+     static void UpdateCB ()
+     {
+         // Check here
+         GameObject[] go = Object.FindObjectsOfType (typeof(GameObject)) as GameObject[];
+         
+         markedObjects = new List<int> ();
+         foreach (GameObject g in go) 
+         {
+             // Example: mark all lights
+             if (g.GetComponent<NoteComponent> () != null)
+                 markedObjects.Add (g.GetInstanceID ());
+         }
+         
+     }
+ 
+     static void HierarchyItemCB (int instanceID, Rect selectionRect)
+     {
+         // place the icoon to the right of the list:
+         Rect r = new Rect (selectionRect); 
+         r.x = r.width - 20;
+         r.width = 18;
+         
+         if (markedObjects.Contains (instanceID)) 
+         {
+             // Draw the texture if it's a light (e.g.)
+             GUI.Label (r, texture); 
+         }
+     }
+     
+ }
