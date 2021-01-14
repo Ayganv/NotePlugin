a56d5d41de8703acb07c37cad2556bc93eb82ae4
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-01-14 11:59:24 +0100

    Adds ReadOnly Serialized Field for "Device name" and "Created time" values in NoteComponent.cs

--- Assets/Scripts/NoteComponent.cs
+++ Assets/Scripts/NoteComponent.cs
@@ -1,11 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NoteComponent : MonoBehaviour
 {
+    public string title;
+    
+    [ReadOnly]
+    [SerializeField] public string deviceName;
+    
+    [ReadOnly]
+    [SerializeField] public string CreatedDate;
+
     [TextArea(10, 10)]
     public string Note;
     public string tags;
     public Color color;
+
+    
+    
+    private NoteComponent()
+    {
+        CreatedDate = DateTime.UtcNow.ToString();
+        deviceName = Environment.UserName;
+    }
 }
